708bb04211444ac0fb0b45ca524443e72181e937
Md Aftab Uddin <aftabhasan7856@gmail.com>
2026-02-28 23:55:48 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: "Aftab", home:MyHome());
+  }
+}
+
+
+
+
+
+
+
+
+
+class MyHome extends StatefulWidget {
+  const MyHome({super.key});
+
+  @override
+  State<MyHome> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyHome> {
+  int count = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Stateful Class"),
+        backgroundColor: Colors.tealAccent,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+          Text(count.toString(),style:TextStyle(
+            fontSize: 30,
+            color: Colors.deepOrange
+          ) ,
+          ),
+  Row(
+  children: [
+   SizedBox( 
+    width: 180,
+
+    child:ElevatedButton(onPressed: (){}, child: Text("+",style: TextStyle(
+      fontSize: 40,
+      color: Colors.black
+    ),)),
+    
+    ),
+SizedBox(width: 10,),
+SizedBox( 
+    width: 180,
+
+    child:ElevatedButton(onPressed: (){}, child: Text("-",style: TextStyle(
+      fontSize: 40,
+      color: Colors.black
+    ),)),
+    
+    ),
+
+    
+  ],
+)
+
+
+        ],),
+      ),
+    );
+  }
+}
