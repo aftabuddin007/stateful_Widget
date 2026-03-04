d6927ad7253cb0e6ce464f147b9185eb9f81d01d
Md Aftab Uddin <aftabhasan7856@gmail.com>
2026-03-04 23:44:48 +0600

    expended working

--- lib/Ratio.dart
+++ lib/Ratio.dart
@@ -19,6 +19,57 @@ class _RatioState extends State<Ratio> {
       body: SingleChildScrollView(
         child: Column(
           children: [
+
+            FractionallySizedBox(
+              child: ElevatedButton(onPressed: (){}, child: Text("Submit")),
+            ),
+
+
+
+
+
+            Row(
+              children: [
+                Flexible(
+                  flex: 3,
+                  child: Container(height: 100, color: Colors.green),
+                ),
+                Flexible(   
+                  flex: 6,
+                  child: Container(
+                    height: 100,
+                    color: const Color.fromARGB(255, 142, 38, 93),
+                  ),
+                ),
+              ],
+            ),
+
+            Container(height: 100, color: Colors.amberAccent),
+            Container(height: 100, color: Color.fromARGB(255, 18, 18, 141)),
+
+            Row(
+              children: [
+                Expanded(
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: Text("Person1"),
+                  ),
+                ),
+                Expanded(
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: Text("Person1"),
+                  ),
+                ),
+                Expanded(
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: Text("Person1"),
+                  ),
+                ),
+              ],
+            ),
+
             AspectRatio(
               aspectRatio: 16 / 9,
               child: Container(color: Colors.deepOrange),
@@ -26,6 +77,14 @@ class _RatioState extends State<Ratio> {
             AspectRatio(
               aspectRatio: 9 / 16,
               child: Container(color: const Color.fromARGB(255, 57, 7, 7)),
+            ),
+            AspectRatio(
+              aspectRatio: 4 / 3,
+              child: Container(color: const Color.fromARGB(255, 7, 138, 70)),
+            ),
+            AspectRatio(
+              aspectRatio: 16 / 10,
+              child: Container(color: const Color.fromARGB(255, 87, 89, 216)),
             ),
           ],
         ),
